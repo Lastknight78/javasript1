91812ab2df372a8a1cf1ecc0181f73e6a6b8b063
Adeniyi <adekunleabioye27@gmail.com>
2022-04-26 06:29:43 -0700

    added java and javascript files

---
+++ java.js
@@ -0,0 +1,92 @@
+// Day 2 exercises
+//exercise level 1
+/*1. declare a variable named challenge and assign to it an initial value of '30 days of javaScript'*/
+let challenge = '30 days of javaScript'
+
+2.console.log(challenge)
+
+3.console.log(challenge.length)
+
+4.console.log(challenge.toUpperCase())
+
+5.console.log(challenge.toLowerCase())
+
+6.console.log(challenge.substring(10,20))
+
+7.console.log(challenge.substring(4))
+
+8.console.log(challenge.includes('Script'))
+
+9.console.log(challenge.split())
+
+10.console.log(challenge.split(' '))
+
+11.console.log('facebook','oracle','twitter','amazon','IBM'.split())
+
+12.console.log(challenge.replace('javaScript','python'))
+
+13.console.log(challenge.charAt(15))
+
+14.console.log(challenge.charCodeAt('j'))
+
+15.console.log(challenge.indexOf('a'))
+
+16.console.log(challenge.lastIndexOf('a'))
+
+let sentence = "you cannot end a sentence with because because because is a conjuntion"
+
+17.console.log(sentence.indexOf('a'))
+
+18.console.log(sentence.lastIndexOf('a'))
+
+19.console.log(sentence.search('because'))
+
+20.console.log(challenge.trim())
+
+21.console.log(challenge.startsWith('30'))
+
+22.console.log(challenge.endsWith('javaScript'))
+
+23.console.log(challenge.match('a'))
+
+24. let cat = '30 days of'
+console.log(cat.concat('javaScript'))
+
+25.console.log(challenge.repeat(2))
+
+//exercise level 2
+
+1.console.log('There is no exercise better for the heart than reaching down and lifting people up')
+
+2.console.log("Love is not patronizing and charity isn't about pity, it is about love.Charity and love are the same thing-- with charity you give love,so don't just give out money but reach out your hand instead")
+
+3.let num = '10'
+let numInt = parseInt(num)
+console.log(numInt)
+
+4. let float = '9.8'
+let numFloat = parseFloat(float)
+console.log(numFloat)
+console.log(Math.round(numFloat))
+
+5.console.log('python'.includes('on')&& 'jargon'.includes('on'))
+
+6.let randomNum = Math.random()
+let numBtn0And100 = randomNum * 101
+console.log(numBtn0And100)
+let numBtn0And100RoundToFloor = Math.floor(numBtn0And100)
+console.log(numBtn0And100RoundToFloor)
+
+7. console.log('i hope this course is not full ofjargon'.includes('jargon'))
+
+8.console.log(sentence.substr(31,54))
+
+9.console.log('Love is the best thing in ths world. some found their love and some are still looking for their love.'.match('love'))
+
+10.console.log(sentence.match('because'))
+
+11.let income = (5000 + 10000 + 15000)
+console.log(income)
+
+
+
